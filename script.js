cfb9b4d98e74f50e1694f61b7480853204e91a41
ThakareSakshi <sakshithakare121@gmail.com>
2023-10-06 15:14:18 +0530

    uploaded assignment

---
+++ script.js
@@ -0,0 +1,17 @@
+let keyb=document.getElementById("key");
+let codeb=document.getElementById("code");
+
+document.addEventListener('keypress',(e)=>{
+    let keytext=e.key;
+    console.log(e.key);
+    let codetext=e.keyCode;
+    var audio = new Audio("./bell.wav");
+    audio.play();
+    keyb.innerHTML=`YOU PRESSED <SPAN>${keytext}</SPAN>`;
+    codeb.innerHTML=`<span>${codetext}</span>`;
+    codeb.style.fontSize=30+"px";
+    codeb.style.padding=30+"px";
+    codeb.style.width=200+"px";
+    codeb.style.backgroundColor="orange";
+    
+})
